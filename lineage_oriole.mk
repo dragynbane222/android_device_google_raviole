1b4d2cb8bb967602acc16f27cd008f923de2537a
Cody Smith <cody.smith9202@gmail.com>
2024-11-26 18:35:59 -0800

    Rise-ify

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -16,6 +16,34 @@ $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
 $(call inherit-product, device/google/gs101/lineage_common.mk)
 $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 
+# Lunch banner maintainer variable
+RISING_MAINTAINER="c_smith"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer) 
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Google Tensor 1" \
+    RisingMaintainer="c_smith"
+
+# chipset flag enclose var with "" if more than one
+# this will reflect on build/display version, a firmware package/zip name 
+# e.g. risingOS-6.0-COMMUNITY-device-AOSP.zip - AOSP is the default package type, WITH_GMS will override the package type to PIXEL
+RISING_PACKAGE_TYPE := "PIXEL"
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# whether to ship aperture camera, default is false
+PRODUCT_NO_CAMERA := false
+
+WITH_GMS := true
+
+TARGET_CORE_GMS := true
+
+TARGET_CORE_GMS_EXTRAS := true
+
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6
